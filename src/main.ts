63f771c68e08bf0ce57978914c788922fa59c188
acil <azisfathl@gmail.com>
2026-04-08 16:00:24 +0700

    add global filter : http exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ZodValidationService } from './common/zod-validation.service';
 import { LoggingInterceptor } from './common/logging/logging.interceptor';
 import { TransformInterceptor } from './common/transform/transform.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     new LoggingInterceptor(),
     new TransformInterceptor(),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
